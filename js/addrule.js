fb7100073b6ad2ac61d462e03f01369ca411179e
chandan2495 <chandan2495@gmail.com>
2015-12-23 23:26:10 +0530

    Toggle ruler on extension icon click and new icons

--- js/addrule.js
+++ js/addrule.js
@@ -1,12 +1,16 @@
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
-    console.log('received');
+    console.log(message.showRuler);
     var horizontalimg = chrome.extension.getURL("img/horizontal.jpg");
     var verticalimg = chrome.extension.getURL("img/vertical.jpg");
     var verticalrule = '<div class="rulers" style="width:100px;height:600px;opacity:0.9;position:absolute;z-index:99999;"><img src=' + verticalimg + ' style="max-width:100%;min-width:100%"></img></div>';
     var horizontalrule = '<div class="rulers" style="width:572px;height:100px;opacity:0.9;position:absolute;z-index:99999;"><img src=' + horizontalimg + ' style="max-width:100%;min-width:100%"></img></div>';
-    if ($('.rulers')) {
+    if (message.showRuler) {
         $('body').prepend(horizontalrule);
         $('body').prepend(verticalrule);
-        $('.rulers').draggable();
+        $('.rulers').draggable();        
     }
+    else{
+        $('.rulers').remove();
+    }
+    sendResponse({showRuler : !message.showRuler});
 });
--- js/background.js
+++ js/background.js
@@ -1,10 +1,12 @@
+var showRuler=true;
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.query({
         active: true,
         currentWindow: true
-    }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {}, function(response) {
-            console.log('response');
+    }, function(tabs) {    	
+        chrome.tabs.sendMessage(tabs[0].id, {showRuler : showRuler}, function(response) {
+            console.log(response.showRuler);
+            showRuler=response.showRuler;            
         });
     });
 });
