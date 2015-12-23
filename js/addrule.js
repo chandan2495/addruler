0b92f251784744316e38e95e14668e8dec2ca0c8
unknown <csingh@csingh.gp.cv.commvault.com>
2015-12-23 18:50:09 +0530

    Ruler image added

--- js/addrule.js
+++ js/addrule.js
@@ -1,6 +1,12 @@
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
     console.log('received');
-    var rule = '<div class="rulers" style="width:100px;height:600px;background:cadetblue;opacity:0.5;position:absolute;z-index:99999;">';
-    $('body').prepend(rule);
-    $('.rulers').draggable();
+    var horizontalimg = chrome.extension.getURL("img/horizontal.jpg");
+    var verticalimg = chrome.extension.getURL("img/vertical.jpg");
+    var verticalrule = '<div class="rulers" style="width:100px;height:600px;opacity:0.9;position:absolute;z-index:99999;"><img src=' + verticalimg + ' style="max-width:100%;min-width:100%"></img></div>';
+    var horizontalrule = '<div class="rulers" style="width:572px;height:100px;opacity:0.9;position:absolute;z-index:99999;"><img src=' + horizontalimg + ' style="max-width:100%;min-width:100%"></img></div>';
+    if ($('.rulers')) {
+        $('body').prepend(horizontalrule);
+        $('body').prepend(verticalrule);
+        $('.rulers').draggable();
+    }
 });
